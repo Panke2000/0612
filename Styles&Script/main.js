e78d663a70bb35993c93af8bc7d6ed015ec327e7
Panke2000 <jovan.panic@pmf.edu.rs>
2023-06-12 08:03:19 +0200

    first commit

---
+++ Styles&Script/main.js
@@ -0,0 +1,15 @@
+
+/* HEADER */
+const header = document.querySelector('header');
+const navbar = document.querySelector('nav');
+const links = document.querySelectorAll('nav a');
+
+window.addEventListener("scroll", function() {
+
+    if (window.scrollY > header.offsetTop) {
+        header.classList.add('header-bg');
+    }else{
+        header.classList.remove('header-bg');
+    }
+
+  });
